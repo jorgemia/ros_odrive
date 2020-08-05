57ecf1201d403eed0998c1559e170102691604f1
jorgemia <jmiarnau97@gmail.com>
2020-08-05 12:02:20 +0100

    Update odrive.hpp

--- include/ros_odrive/odrive.hpp
+++ include/ros_odrive/odrive.hpp
@@ -10,8 +10,8 @@
 #include <string>
 #include <vector>
 #include <libusb-1.0/libusb.h>
-#include "ros_odrive/odrive_msg.h"
-#include "ros_odrive/odrive_ctrl.h"
+//#include "ros_odrive/odrive_msg.h" can get rid of this
+//#include "ros_odrive/odrive_ctrl.h" can get rid of this
 #include <string>
 #include <fstream>
 
@@ -24,21 +24,52 @@
 #define ODRIVE_OK    0
 #define ODRIVE_ERROR 1
 
-// Listener commands
-enum commands {
-    CMD_AXIS_RESET,
-    CMD_AXIS_IDLE,
-    CMD_AXIS_CLOSED_LOOP,
-    CMD_AXIS_SET_VELOCITY,
-    CMD_REBOOT
-};
+// Listener commands - Dont think I need this?
+//enum commands {
+//    CMD_AXIS_RESET,
+//    CMD_AXIS_IDLE,
+//    CMD_AXIS_CLOSED_LOOP,
+//    CMD_AXIS_SET_VELOCITY,
+//    CMD_REBOOT
+//};
 
-class odrive{
+namespace odrive
+{
 
-    private:
-        void msgCallback(const ros_odrive::odrive_ctrl::ConstPtr& msg);
+    class ODriveDriver {
 
     public:
+        ODriveDriver(
+                const std::string* odrive_serial_numbers,
+                const uint8_t num_odrives,
+                const std::string* motor_to_odrive_serial_number_map,
+                const uint8_t* motor_index_map,
+                const uint8_t num_motors); 
+        ~ODriveDriver();
 
-};
+        int init(); // start communication
+
+        int getPosCPR(int motor_index, float &pos);
+
+        int setMotorSpeed(int motor_index, float motor_speed);
+        int setMotorSpeeds(float* motor_speeds); // assumed to match num_motors
+
+        int getMotorSpeed(int motor_index, float &motor_speed);
+        int getBusVoltage(int motor_index, float &voltage);
+        
+        int checkErrors(uint8_t* error_codes_array); // assumed to match num_motors
+
+    private:
+        // read settings
+        uint8_t num_odrives_;
+        uint8_t num_motors_;
+
+        // saved for use between creation and init
+        std::string* odrive_serial_numbers_;
+        std::string* motor_to_odrive_serial_number_map_;
+        uint8_t* motor_index_map_;
+
+    };
+}
+
 #endif
